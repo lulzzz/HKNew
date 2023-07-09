d8a80ee74f5df447027149a571fded42eafd9397
ObsDIan <a29879350@gmail.com>
2023-07-10 01:14:38 +0800

    fix_ig_upload

---
+++ HK_project/Extensions/JsonExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace HK_Project.Extensions
+{
+    public static class JsonExtension
+    {
+        public static string ToJson(this object obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+        public static T ToObject<T>(this string json)
+        {
+            //if (string.IsNullOrEmpty(json))
+            //{
+            //    return default(T);
+            //}
+            //return JsonSerializer.Deserialize<T>(json);
+            try
+            {
+                return JsonSerializer.Deserialize<T>(json);
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+    }
+}
---
+++ HK_project/Extensions/VectorOperationExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace HK_Project.Extensions
+{
+    public static class VectorOperationExtension
+    {
+        public static float InnerProduct(this IEnumerable<float> vector1, IEnumerable<float> vector2)
+        {
+            return vector1.Zip(vector2, (a, b) => a * b).Sum();
+        }
+
+        public static float CosineSimilarity(this IEnumerable<float> vector1, IEnumerable<float> vector2)
+        {
+            return vector1.InnerProduct(vector2) / (float)(Math.Sqrt(vector1.InnerProduct(vector1)) * Math.Sqrt(vector2.InnerProduct(vector2)));
+        }
+    }
+}
