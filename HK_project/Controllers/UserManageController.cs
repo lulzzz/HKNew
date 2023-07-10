937e43a68b566668bdf3fe38e485d3e4899b3bd9
max910815 <a0966474739@gmail.com>
2023-07-10 19:54:45 +0800

    07/10    07:54

---
+++ HK_project/Controllers/UserManageController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HK_Project.ViewModels;
+using HKDB.Models;
+using HKDB.Data;
+using System.Security.Claims;
+using HK_Project.Services;
+
+namespace HK_Project.Controllers
+{
+    public class UserManageController : Controller
+    {
+        private readonly HKContext _ctx;
+        private readonly ClaimServer _claim;
+        public UserManageController(HKContext ctx, ClaimServer c)
+        {
+            _ctx = ctx;
+            _claim = c;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> UserLoginSingup(UserLoginSinginViewModel lvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var MemberEmail_exist = _ctx.Members.FirstOrDefault(m => m.MemberEmail == lvm.Email);
+                var UserEmail_exist = _ctx.Users.FirstOrDefault(u => u.UserEmail == lvm.Email);
+
+                if(UserEmail_exist == null)
+                {
+                    //使用者跟會員都沒有紀錄
+                    User user = new User()
+                    {
+                        UserEmail = lvm.Email
+                    };
+                    _ctx.Users.Add(user);
+                    await _ctx.SaveChangesAsync();
+                }
+                _claim.ClaimAdd(lvm.Email);
+                
+                return RedirectToAction("Index", "Home");
+            }
+            return View(lvm);
+        }
+
+
+
+
+
+    }
+}
---
+++ HK_project/ViewModels/ClaimViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HK_Project.ViewModels
+{
+    public class ClaimViewModel
+    {
+        public string Email { get; set; }
+    }
+}
