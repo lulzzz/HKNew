b11b9299a5d65d67b2c73c693f269693d7c2f00f
林智珉 <eriksonop@gmail.com>
2023-07-18 03:32:56 +0800

    sql

--- HKDB/Data/HKContext.cs
+++ HKDB/Data/HKContext.cs
@@ -27,7 +27,9 @@ namespace HKDB.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
+                //optionsBuilder.UseSqlServer("Data Source=tcp:hackerdb.database.windows.net,1433;Initial Catalog=openAPI_db;User Id=AsiaTeam@hackerdb;Password=Weareloser!");
                 optionsBuilder.UseSqlServer("Data Source=tcp:hackerdb.database.windows.net,1433;Initial Catalog=openAPI_db;User Id=AsiaTeam@hackerdb;Password=Weareloser!");
+                //optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=HKDB;TrustServerCertificate=true;MultipleActiveResultSets=true;Trusted_Connection=True;");
                 base.OnConfiguring(optionsBuilder);
             }
         }
